348ddcfcfe64a510400b23722ecbdf71e77bd66b
FaintGhost <zhang.yaowei@live.com>
2026-02-05 17:41:10 +0800

    feat(panel): wire server startup

--- panel/cmd/panel/main.go
+++ panel/cmd/panel/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+  "log"
+
+  "sboard/panel/internal/api"
+  "sboard/panel/internal/config"
+  "sboard/panel/internal/db"
+)
+
+func main() {
+  cfg := config.Load()
+  database, err := db.Open(cfg.DBPath)
+  if err != nil {
+    log.Fatal(err)
+  }
+  if err := db.MigrateUp(database, "internal/db/migrations"); err != nil {
+    log.Fatal(err)
+  }
+  r := api.NewRouter()
+  if err := r.Run(cfg.HTTPAddr); err != nil {
+    log.Fatal(err)
+  }
+}
